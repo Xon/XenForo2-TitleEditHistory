aa142e14f9e202e40e0079ca19ac70d2db063e61
yugensoft <contact@yugensoft.com>
2018-02-13 18:21:09 +0700

    requested revisions

--- upload/src/addons/SV/TitleEditHistory/EditHistory/Thread.php
+++ upload/src/addons/SV/TitleEditHistory/EditHistory/Thread.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SV\TitleEditHistory\EditHistoryHandler;
+namespace SV\TitleEditHistory\EditHistory;
 
 use XF\EditHistory\AbstractHandler;
 use XF\Mvc\Entity\Entity;
@@ -66,23 +66,24 @@ class Thread extends AbstractHandler
 	 * @return mixed
 	 */
 	public function revertToVersion(Entity $content, \XF\Entity\EditHistory $history, \XF\Entity\EditHistory $previous = null) {
-		$content->set('title', $history->old_text);
-		$content->set('thread_title_edit_count', $content->get('thread_title_edit_count') + 1);
-		if ($content->get('thread_title_edit_count'))
+		/** @var \SV\TitleEditHistory\XF\Service\Thread\Editor $editor */
+		$editor = \XF::app()->service('XF:Thread\Editor', $content);
+
+		$editor->logEdit(false);
+		$editor->setTitle($history->old_text);
+
+		if (!$previous || $previous->edit_user_id != $content->user_id)
 		{
-			if (!$previous || $previous->edit_user_id != $content->user_id)
-			{
-				// if previous is a mod edit, don't show as it may have been hidden
-				$content->set('thread_title_last_edit_date', 0);
-			}
-			else if ($previous && $previous->edit_user_id == $content->user_id)
-			{
-				$content->set('thread_title_last_edit_date', $previous->edit_date);
-				$content->set('thread_title_last_edit_user_id', $previous->edit_user_id);
-			}
+			// if previous is a mod edit, don't show as it may have been hidden
+			$content->thread_title_last_edit_date = 0;
+		}
+		else if ($previous && $previous->edit_user_id == $content->user_id)
+		{
+			$content->thread_title_last_edit_date = $previous->edit_date;
+			$content->thread_title_last_edit_user_id = $previous->edit_user_id;
 		}
 
-		return $content->save();
+		return $editor->save();
 	}
 
 	public function getHtmlFormattedContent($text, Entity $content = null) {
--- upload/src/addons/SV/TitleEditHistory/XF/Entity/Thread.php
+++ upload/src/addons/SV/TitleEditHistory/XF/Entity/Thread.php
@@ -31,65 +31,6 @@ class Thread extends XFCP_Thread {
 		return false;
 	}
 
-	protected function _isNotInsideSilentEditWindow() {
-		return \XF::$time - $this->get('post_date') > \XF::options()->editLogDisplay['delay'] * 60;
-	}
-
-	protected function _preSave() {
-		$upstream = parent::_preSave();
-
-		if ($this->isUpdate() && $this->isChanged('title') && $this->_isNotInsideSilentEditWindow())
-		{
-			if (!$this->isChanged('thread_title_last_edit_date'))
-			{
-				$this->set('thread_title_last_edit_date', \XF::$time);
-
-				if (!$this->isChanged('thread_title_last_edit_user_id'))
-				{
-					$this->set('thread_title_last_edit_user_id', \XF::visitor()->user_id);
-				}
-			}
-
-			if (!$this->isChanged('thread_title_edit_count'))
-			{
-				$this->set('thread_title_edit_count', $this->get('thread_title_edit_count') + 1);
-			}
-		}
-		if ($this->isChanged('thread_title_edit_count') && $this->get('thread_title_edit_count') == 0)
-		{
-			$this->set('thread_title_last_edit_date', 0);
-		}
-		if (!$this->get('thread_title_last_edit_date'))
-		{
-			$this->set('thread_title_last_edit_user_id', 0);
-		}
-
-		return $upstream;
-	}
-
-	protected function _postSave() {
-		$upstream = parent::_postSave();
-
-		if ($this->isUpdate() && $this->isChanged('title') && $this->_isNotInsideSilentEditWindow())
-		{
-			$this->_insertTitleEditHistory();
-		}
-
-		return $upstream;
-	}
-
-	protected function _insertTitleEditHistory()
-	{
-		$history = \XF::em()->create('XF:EditHistory');
-		$history->bulkSet([
-			'content_type' => 'thread_title',
-			'content_id'   => $this->thread_id,
-			'edit_user_id' => \XF::visitor()->user_id,
-			'old_text'     => $this->getExistingValue('title')
-		]);
-		$history->save();
-	}
-
 	public static function getStructure(Structure $structure) {
 		$structure = parent::getStructure($structure);
 
