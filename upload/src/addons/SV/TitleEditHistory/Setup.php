e2a6434000fc479f1a5423328e7d748f765b3eb8
Xon <tazipper1@hotmail.com>
2018-03-26 19:39:28 +0800

    Rebuild installer to follow standard pattern

--- upload/src/addons/SV/TitleEditHistory/Setup.php
+++ upload/src/addons/SV/TitleEditHistory/Setup.php
@@ -7,6 +7,7 @@ use XF\AddOn\StepRunnerInstallTrait;
 use XF\AddOn\StepRunnerUninstallTrait;
 use XF\AddOn\StepRunnerUpgradeTrait;
 use XF\Db\Schema\Alter;
+use XF\Db\Schema\Create;
 
 class Setup extends AbstractSetup
 {
@@ -16,22 +17,16 @@ class Setup extends AbstractSetup
 
     public function installStep1()
     {
-        $this->schemaManager()->alterTable('xf_thread', function (Alter $table) {
-            $table->addColumn('thread_title_edit_count')->type('int')->nullable(false)->setDefault(0);
-            $table->addColumn('thread_title_last_edit_date')->type('int')->nullable(false)->setDefault(0);
-            $table->addColumn('thread_title_last_edit_user_id')->type('int')->nullable(false)->setDefault(0);
-        });
-    }
+        $sm = $this->schemaManager();
 
-    public function installStep2()
-    {
-        if ($this->resourceManagerInstalled())
+        foreach ($this->getTables() as $tableName => $callback)
         {
-            $this->schemaManager()->alterTable('xf_rm_resource', function (Alter $table) {
-                $table->addColumn('resource_title_edit_count')->type('int')->nullable(false)->setDefault(0);
-                $table->addColumn('resource_title_last_edit_date')->type('int')->nullable(false)->setDefault(0);
-                $table->addColumn('resource_title_last_edit_user_id')->type('int')->nullable(false)->setDefault(0);
-            });
+            $sm->createTable($tableName, $callback);
+        }
+
+        foreach ($this->getAlterTables() as $tableName => $callback)
+        {
+            $sm->alterTable($tableName, $callback);
         }
     }
 
@@ -50,53 +45,130 @@ class Setup extends AbstractSetup
         });
     }
 
-    public function upgrade2000070Step1()
+    public function upgrade2010000Step1()
     {
-        if ($this->resourceManagerInstalled())
-        {
-            $this->schemaManager()->alterTable('xf_rm_resource', function (Alter $table) {
-                $table->addColumn('resource_title_edit_count')->type('int')->nullable(false)->setDefault(0);
-                $table->addColumn('resource_title_last_edit_date')->type('int')->nullable(false)->setDefault(0);
-                $table->addColumn('resource_title_last_edit_user_id')->type('int')->nullable(false)->setDefault(0);
-            });
-        }
+        $this->installStep1();
     }
 
     public function uninstallStep1()
     {
-        $this->db()->query(
-            "
-            DELETE FROM xf_edit_history
-            WHERE content_type = 'thread_title'
-        "
-        );
+        $sm = $this->schemaManager();
+
+        foreach ($this->getTables() as $tableName => $callback)
+        {
+            $sm->dropTable($tableName);
+        }
     }
 
     public function uninstallStep2()
     {
-        $this->schemaManager()->alterTable('xf_thread', function (Alter $table) {
-            $table->dropColumns(['thread_title_edit_count', 'thread_title_last_edit_date', 'thread_title_last_edit_user_id']);
-        });
+        $sm = $this->schemaManager();
+
+        foreach ($this->getRemoveAlterTables() as $tableName => $callback)
+        {
+            $sm->alterTable($tableName, $callback);
+        }
     }
 
     public function uninstallStep3()
     {
-        $db = $this->db();
-        $db->delete('xf_edit_history', 'content_type = ' . $db->quote('resource_title'));
-    }
-
-    public function uninstallStep4()
-    {
-        if ($this->resourceManagerInstalled())
-        {
-            $this->schemaManager()->alterTable('xf_rm_resource', function (Alter $table) {
-                $table->dropColumns(['resource_title_edit_count', 'resource_title_last_edit_date', 'resource_title_last_edit_user_id']);
-            });
-        }
+        $this->db()->query(
+            "
+            DELETE FROM xf_edit_history
+            WHERE content_type IN (
+              'thread_title',
+              'resource_title'
+              )
+        "
+        );
     }
 
     protected function resourceManagerInstalled()
     {
         return $this->schemaManager()->tableExists('xf_rm_resource');
+    }
+
+
+    /**
+     * @param Create|Alter $table
+     * @param string       $name
+     * @param string|null  $type
+     * @param string|null  $length
+     * @return \XF\Db\Schema\Column
+     */
+    protected function addOrChangeColumn($table, $name, $type = null, $length = null)
+    {
+        if ($table instanceof Create)
+        {
+            $table->checkExists(true);
+
+            return $table->addColumn($name, $type, $length);
+        }
+        else if ($table instanceof Alter)
+        {
+            if ($table->getColumnDefinition($name))
+            {
+                return $table->changeColumn($name, $type, $length);
+            }
+
+            return $table->addColumn($name, $type, $length);
+        }
+        else
+        {
+            throw new \LogicException("Unknown schema DDL type " . get_class($table));
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function getTables()
+    {
+        $tables = [];
+
+        return $tables;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getAlterTables()
+    {
+        $tables = [];
+
+        $tables['xf_thread'] = function (Alter $table) {
+            $this->addOrChangeColumn($table, 'thread_title_edit_count')->type('int')->nullable(false)->setDefault(0);
+            $this->addOrChangeColumn($table, 'thread_title_last_edit_date')->type('int')->nullable(false)->setDefault(0);
+            $this->addOrChangeColumn($table, 'thread_title_last_edit_user_id')->type('int')->nullable(false)->setDefault(0);
+        };
+
+        if ($this->resourceManagerInstalled())
+        {
+            $tables['xf_rm_resource'] = function (Alter $table) {
+                $this->addOrChangeColumn($table, 'resource_title_edit_count')->type('int')->nullable(false)->setDefault(0);
+                $this->addOrChangeColumn($table, 'resource_title_last_edit_date')->type('int')->nullable(false)->setDefault(0);
+                $this->addOrChangeColumn($table, 'resource_title_last_edit_user_id')->type('int')->nullable(false)->setDefault(0);
+            };
+        }
+
+        return $tables;
+    }
+
+    protected function getRemoveAlterTables()
+    {
+        $tables = [];
+
+        $tables['xf_thread'] = function (Alter $table) {
+            $table->dropColumns(['thread_title_edit_count', 'thread_title_last_edit_date', 'thread_title_last_edit_user_id']);
+        };
+
+        if ($this->resourceManagerInstalled())
+        {
+            $tables['xf_rm_resource'] = function (Alter $table) {
+                $table->dropColumns(['resource_title_edit_count', 'resource_title_last_edit_date', 'resource_title_last_edit_user_id']);
+            };
+        }
+
+        return $tables;
     }
 }
