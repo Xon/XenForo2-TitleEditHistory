02c7573a79c5a03437eb32e729c11e7fdf746d02
ticktackk <31729605+ticktackk@users.noreply.github.com>
2018-03-27 09:41:48 +0530

    Add EditorTrait for Edit services

---
+++ upload/src/addons/SV/TitleEditHistory/Service/Base/EditorTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SV\TitleEditHistory\Service\Base;
+
+use XF\Mvc\Entity\Entity;
+use XF\Entity\User;
+
+trait EditorTrait
+{
+    /** @var bool */
+    protected $logEdit = true;
+    /** @var null|string */
+    protected $oldTitle = null;
+    /** @var null|int */
+    protected $logDelay = null;
+    /** @var bool */
+    protected $logHistory = true;
+
+    /**
+     * @param $logEdit
+     */
+    public function logEdit($logEdit)
+    {
+        $this->logEdit = $logEdit;
+    }
+
+    /**
+     * @param $logDelay
+     */
+    public function logDelay($logDelay)
+    {
+        $this->logDelay = $logDelay;
+    }
+
+    /**
+     * @param bool $logHistory
+     */
+    public function logHistory($logHistory)
+    {
+        $this->logHistory = $logHistory;
+    }
+}
