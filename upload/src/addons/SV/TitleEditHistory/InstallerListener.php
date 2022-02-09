8248476b3bd016adaf3976a2d598deb06943adc9
TickTackk <me@ticktackk.io>
2022-02-09 09:53:11 +0530

    Call install step 2 after install step 1 when postInstall and postRebuild code even listeners are triggered for supported add-ons

--- upload/src/addons/SV/TitleEditHistory/InstallerListener.php
+++ upload/src/addons/SV/TitleEditHistory/InstallerListener.php
@@ -3,24 +3,59 @@
 namespace SV\TitleEditHistory;
 
 use XF\AddOn\AddOn;
+use XF\Entity\AddOn as AddOnEntity;
 
+/**
+ * @version 2.3.1
+ */
 class InstallerListener
 {
+    /**
+     * @version 2.3.1
+     *
+     * @param AddOn $addOn
+     * @param AddOnEntity $installedAddOn
+     * @param array $json
+     *
+     * @return void
+     */
     public static function addonPostRebuild(/** @noinspection PhpUnusedParameterInspection */
-        AddOn $addOn, \XF\Entity\AddOn $installedAddOn, array $json)
+        AddOn $addOn,
+        AddOnEntity $installedAddOn,
+        array $json
+    )
     {
-        if (empty(Setup::$supportedAddOns[$addOn->getAddOnId()]))
-        {
-            return;
-        }
-
-        // kick off the installer
-        $setup = new Setup($addOn, \XF::app());
-        $setup->installStep1();
+        static::runInstallSteps($addOn);
     }
 
+    /**
+     * @version 2.3.1
+     *
+     * @param AddOn $addOn
+     * @param AddOnEntity $installedAddOn
+     * @param array $json
+     * @param array $stateChanges
+     *
+     * @return void
+     */
     public static function addonPostInstall(/** @noinspection PhpUnusedParameterInspection */
-        AddOn $addOn, \XF\Entity\AddOn $installedAddOn, array $json, array &$stateChanges)
+        AddOn $addOn,
+        AddOnEntity $installedAddOn,
+        array $json,
+        array &$stateChanges
+    )
+    {
+        static::runInstallSteps($addOn);
+    }
+
+    /**
+     * @since 2.3.1
+     *
+     * @param AddOn $addOn
+     *
+     * @return void
+     */
+    protected static function runInstallSteps(AddOn $addOn)
     {
         if (empty(Setup::$supportedAddOns[$addOn->getAddOnId()]))
         {
@@ -30,5 +65,6 @@ class InstallerListener
         // kick off the installer
         $setup = new Setup($addOn, \XF::app());
         $setup->installStep1();
+        $setup->installStep2();
     }
 }
